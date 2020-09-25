4c5130370060bb3686a1021eb18f447ddb0eb678
Jonathan <jonathanmayceh@gmail.com>
2020-09-25 09:37:20 -0500

    Se separan las rutas de sus funciones

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,59 +1,9 @@
-const express = require('express');
-const app = express();
-const Usuario = require('../models/usuario');
+const { Router } = require('express');
+const router = Router();
+const { listar, registrar } = require('../controllers/controller');
 
-app.get('/listar', (req, res) => {
-    Usuario.find({}).exec((err, usuarios) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
+router.get('/listar', listar)
 
-        res.json({
-            ok: true,
-            usuarios
-        });
-    })
-})
+router.post('/registrar', registrar);
 
-app.post('/registrar', (req, res) => {
-
-    let body = req.body;
-
-    let usuario = new Usuario({
-        nombre: body.nombre,
-        email: body.email,
-        kilometros: body.kilometros
-    });
-
-    if (usuario.kilometros > 4) {
-
-        usuario.save((err, usuarioDB) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-
-            res.json({
-                ok: true,
-                usuario: usuarioDB
-            });
-        });
-
-    } else {
-        // console.log('hay que caminar más');
-        return res.json({
-            ok: false,
-            message: 'Debes caminar más'
-        })
-    }
-
-
-
-});
-
-module.exports = app;
+module.exports = router;
